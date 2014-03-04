18154366d4fbeda8dea3d957fe59db2a65c15257
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2014-03-04 21:58:29 +0330

    Create tl_module.php

---
+++ languages/en/tl_module.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   prayertime
+ * @author    Hamid Abbaszadeh
+ * @license   GNU/LGPL
+ * @copyright 2014
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['pt_city']           = array('City name','');
+$GLOBALS['TL_LANG']['tl_module']['pt_timezone']       = array('Time zone','');
+$GLOBALS['TL_LANG']['tl_module']['pt_latitude']       = array('Latitude','');
+$GLOBALS['TL_LANG']['tl_module']['pt_longitude']      = array('Longitude','');
+$GLOBALS['TL_LANG']['tl_module']['pt_CalcMethod']     = array('Calculation Method','');
+$GLOBALS['TL_LANG']['tl_module']['pt_AsrMethod']      = array('Juristic Method','');
+$GLOBALS['TL_LANG']['tl_module']['pt_HighLatsMethod'] = array('Higher Latitudes Method','');
+$GLOBALS['TL_LANG']['tl_module']['pt_TimeFormat']     = array('Time format','');
+$GLOBALS['TL_LANG']['tl_module']['pt_DateFormat']     = array('Date format','');
+$GLOBALS['TL_LANG']['tl_module']['pt_DateType']       = array('Date type','');
+$GLOBALS['TL_LANG']['tl_module']['pt_prayertime']      = array('Prayer','');
+
+
+
+$GLOBALS['TL_LANG']['tl_module']['city_legend']    = 'City';
+$GLOBALS['TL_LANG']['tl_module']['methods_legend'] = 'Methods';
+$GLOBALS['TL_LANG']['tl_module']['options_legend'] = 'Options';
+$GLOBALS['TL_LANG']['tl_module']['prayer_legend']  = 'Prayer times';
