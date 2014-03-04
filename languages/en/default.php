e484d569f7e936c5f43d948fda45ee75c651fd00
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2014-03-04 21:57:24 +0330

    Create default.php

---
+++ languages/en/default.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   prayertime
+ * @author    Hamid Abbaszadeh
+ * @license   GNU/LGPL
+ * @copyright 2014
+ */
+
+
+/**
+ * Miscellaneous
+ */
+$GLOBALS['TL_LANG']['MSC']['today'] = 'Today';
+$GLOBALS['TL_LANG']['MSC']['tomarrow'] = 'Tomarrow';
+$GLOBALS['TL_LANG']['MSC']['Fajr'] = 'Fajr';
+$GLOBALS['TL_LANG']['MSC']['Sunrise'] = 'Sunrise';
+$GLOBALS['TL_LANG']['MSC']['Dhuhr'] = 'Dhuhr';
+$GLOBALS['TL_LANG']['MSC']['Asr'] = 'Asr';
+$GLOBALS['TL_LANG']['MSC']['Sunset'] = 'Sunset';
+$GLOBALS['TL_LANG']['MSC']['Maghrib'] = 'Maghrib';
+$GLOBALS['TL_LANG']['MSC']['Isha'] = 'Isha';
