7dbd7110a1eee18682589514831eab5616712569
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2014-03-04 21:56:00 +0330

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package   prayertime
+ * @author    Hamid Abbaszadeh
+ * @license   GNU/LGPL
+ * @copyright 2014
+ */
+
+/**
+ * Front end modules
+ */
+$GLOBALS['FE_MOD']['miscellaneous']['prayertime']   = 'ModulePrayerTime';
