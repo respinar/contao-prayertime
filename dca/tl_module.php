866bc55c061f993e254c7480243a0b3a7a44dabc
Hamid Abbaszadeh <abbaszadeh.h@gmail.com>
2016-09-03 23:18:08 +0430

    Change timezone

--- dca/tl_module.php
+++ dca/tl_module.php
@@ -1,20 +1,18 @@
 <?php
 
 /**
- * Contao Open Source CMS
+ * praytimes Extension for Contao Open Source CMS
  *
- * Copyright (c) 2005-2014 Leo Feyer
- *
- * @package   prayertime
- * @author    Hamid Abbaszadeh
- * @license   GNU/LGPL
- * @copyright 2014
+ * @copyright  Copyright (c) 2014-2016, Respinar
+ * @author     respinar <info@respinar.com>
+ * @license    http://opensource.org/licenses/lgpl-3.0.html LGPL
+ * @link       https://github.com/respinar/contao-praytimes
  */
 
-$GLOBALS['TL_DCA']['tl_module']['palettes']['prayertime']   = '{title_legend},name,headline,type;
+$GLOBALS['TL_DCA']['tl_module']['palettes']['praytimes']   = '{title_legend},name,headline,type;
                                                                {city_legend},pt_city,pt_timezone,pt_latitude,pt_longitude;
                                                                {methods_legend},pt_CalcMethod,pt_AsrMethod,pt_HighLatsMethod;
-                                                               {prayer_legend},pt_prayertime;
+                                                               {praytime_legend},pt_praytimes;
                                                                {options_legend},pt_TimeFormat,pt_DateFormat;
                                                                {protected_legend:hide},protected;
                                                                {expert_legend:hide},guests,cssID,space';
@@ -35,8 +33,9 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['pt_timezone'] = array
 (
 	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['pt_timezone'],
 	'exclude'                 => true,
-	'inputType'               => 'text',
-	'eval'                    => array('rgxp'=>'digit','tl_class'=>'w50'),
+	'inputType'               => 'select',
+	'options'                 => System::getTimeZones(),
+	'eval'                    => array('chosen'=>true, 'tl_class'=>'w50'),
 	'sql'                     => "varchar(128) NOT NULL default '0'"
 );
 $GLOBALS['TL_DCA']['tl_module']['fields']['pt_latitude'] = array
@@ -105,9 +104,9 @@ $GLOBALS['TL_DCA']['tl_module']['fields']['pt_DateFormat'] = array
 	'eval'                    => array('tl_class'=>'w50'),
 	'sql'                     => "varchar(128) NOT NULL default 'l d F'"
 );
-$GLOBALS['TL_DCA']['tl_module']['fields']['pt_prayertime'] = array
+$GLOBALS['TL_DCA']['tl_module']['fields']['pt_praytimes'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['pt_prayertime'],
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['pt_praytimes'],
 	'default'                 => array('Fajr','Sunrise','Dhuhr','Sunset','Maghrib'),
 	'exclude'                 => true,
 	'inputType'               => 'checkbox',
